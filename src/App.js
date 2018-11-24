01873f7274c09caeb11976533b8776a82522ddc2
matzieq <mjp.kozlowski@gmail.com>
2018-11-24 14:26:38 +0100

    First steps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Maciek", age: 36},
+      { name: "Zenek", age: 54}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>To działaa!</h1>
+        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
+        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>My hobbies: none</Person>
+        
       </div>
     );
   }
